ba2b5cf3f1ed762db69790a3643573d4e3c63bd1
ryudaibaba <1844ryudai@gmail.com>
2021-06-01 00:58:39 +0900

    ユーザーのseeder追加

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(User::class, 50)->create();
+
+        factory(User::class, 2)->create([
+            'role' => '管理者'
+        ]);
+    }
+}
