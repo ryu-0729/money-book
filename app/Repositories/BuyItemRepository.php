32c222fb5532f1158216fd7d1ac09bc85b0a1ca1
ryudaibaba <1844ryudai@gmail.com>
2021-06-09 19:41:29 +0900

    引き続き検証

---
+++ app/Repositories/BuyItemRepository.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\BuyItem; // BuyItemをインポート
+use Illuminate\Support\Facades\Auth;
+
+class BuyItemRepository implements RepositoryInterface
+{
+    public function getAll()
+    {
+        $buyItems = Auth::user()->buyItems()
+            ->select('id', 'name', 'quantity', 'price', 'month')
+            ->paginate(20);
+
+        return $buyItems;
+    }
+
+    // 検索月からその月のデータの取得
+    public function searchMonth($month)
+    {
+        $buyItems = Auth::user()->buyItems()
+            ->select('id', 'name', 'quantity', 'price', 'month')
+            ->searchMonth($month)
+            ->paginate(20);
+
+        return $buyItems;
+    }
+
+    // 検索月からその月の合計金額を取得
+    public function getTotalPrice($month)
+    {
+        $price = Auth::user()->buyItems()
+            ->searchMonth($month)->get(['price', 'month'])->sum('price');
+
+        return $price;
+    }
+}
