4cbfa852b11f3422761995f0b6faf3af9c670620
ryudaibaba <1844ryudai@gmail.com>
2021-08-21 15:48:46 +0900

    システム概要ページの追加

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,6 +17,40 @@
                     @endguest
                 </div>
             </div>
+
+            <div class="text-center text-success" style="margin-top: 50px;">
+                <h1>MoneyBookでお金の管理をしよう！</h1>
+                <h2>月ごとに使った金額を集計してくれる！</h2>
+            </div>
+
+            <div class="card" style="margin-bottom: 20px;">
+                <img src="{{ asset('images/商品登録.png') }}" class="rounded mx-auto d-block" width="100%" height="300">
+                <div class="card-body">
+                    <h5 class="card-title">1.商品登録</h5>
+                    <p class="card-text">商品名、1個単価の金額を登録！</p>
+                    <p class="card-text"><small class="text-muted">タグ付けは後でも大丈夫！</small></p>
+                </div>
+            </div>
+
+            <div class="card" style="margin-bottom: 20px;">
+            <img src="{{ asset('images/購入商品登録.png') }}" class="rounded mx-auto d-block" width="100%" height="300">
+                <div class="card-body">
+                    <h5 class="card-title">2.購入商品登録</h5>
+                    <p class="card-text">1.で登録していた商品を選択！</p>
+                    <p class="card-text">その他、購入個数と購入月を入力！</p>
+                    <p class="card-text"><small class="text-muted">次で最後！</small></p>
+                </div>
+            </div>
+
+            <div class="card" style="margin-bottom: 20px;">
+            <img src="{{ asset('images/金額集計.png') }}" class="rounded mx-auto d-block" width="100%" height="400">
+                <div class="card-body">
+                    <h5 class="card-title">3.金額集計</h5>
+                    <p class="card-text">金額集計したい月を選択して金額集計ボタンをクリック！</p>
+                    <p class="card-text">使った金額を集計してくれる！</p>
+                    <p class="card-text"><small class="text-muted">タグごとで金額を見ることもできる！</small></p>
+                </div>
+            </div>
         </div>
     </div>
 </div>
