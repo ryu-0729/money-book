99d5d6cc3ad7c80a4249c344dd3438ad696bea78
ryudaibaba <1844ryudai@gmail.com>
2021-08-22 18:41:56 +0900

    ユーザーのマイページによく購入する商品の表示

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -19,6 +19,15 @@
                     <a href="{{ route('users.edit', $user->id) }}" class="btn btn-primary">{{ __('編集') }}</a>
                 </div>
             </div>
+            <div class="card bg-light bg-primary mb-3 mx-auto text-center" style="margin-top: 20px;">
+                <div class="card-header">{{ __('最近の購入商品確認') }}</div>
+                <div class="card-body">
+                    <h5 class="card-title">最近購入登録した商品の確認ができます</h5>
+                    <p class="card-text">
+                        <a href="{{ route('history') }}" class="btn btn-primary">{{ __('購入登録履歴へ') }}</a>
+                    </p>
+                </div>
+            </div>
         </div>
     </div>
 </div>
