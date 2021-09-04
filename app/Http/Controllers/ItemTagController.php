bf64c4bad065fa404e9670cddcdd433d5be90cd9
ryudaibaba <1844ryudai@gmail.com>
2021-09-04 21:47:29 +0900

    タグ登録を複数選択できるように変更（複数タグ選択の処理修正）

--- app/Http/Controllers/ItemTagController.php
+++ app/Http/Controllers/ItemTagController.php
@@ -144,7 +144,8 @@ class ItemTagController extends Controller
                     // 購入商品にも同名の購入商品登録がされていた場合、item_tag_nameの更新をする
                     foreach ($buyItems as $buyItem) {
                         BuyItem::where('id', $buyItem->id)->update([
-                            'item_tag_name' => $tagName->tag_name,
+                            'item_tag_name'     => $tagName->tag_name,
+                            'sub_item_tag_name' => null,
                         ]);
                     }
                 }
