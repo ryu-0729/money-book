ec9a000fbe4734b59ec440787d45ffdc6d97875f
ryudaibaba <1844ryudai@gmail.com>
2021-09-04 22:08:39 +0900

    タグ登録を複数選択できるように変更（金額集計ページの修正）

--- app/Repositories/BuyItemRepository.php
+++ app/Repositories/BuyItemRepository.php
@@ -32,7 +32,7 @@ class BuyItemRepository implements RepositoryInterface
     public function getBuyItemDataSearchMonthAndTagName($month, $tagName = null)
     {
         $buyItems = Auth::user()->buyItems()
-            ->select('id', 'name', 'quantity', 'price', 'month', 'item_tag_name', 'updated_at')
+            ->select('id', 'name', 'quantity', 'price', 'month', 'item_tag_name', 'sub_item_tag_name', 'updated_at')
             ->sortable()
             ->searchMonthAndTagName($month, $tagName)
             ->latest('updated_at')
